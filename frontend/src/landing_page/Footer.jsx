7e9c8449debb3ee826d911d7a2def5ab8271419a
BEAST <imyogeshgaikwad@gmail.com>
2026-02-18 18:03:23 +0000

    Footer implementation started

--- frontend/src/landing_page/Footer.jsx
+++ frontend/src/landing_page/Footer.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 
 function Footer() {
     return ( 
-        <h1>Footer</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <img src='../media/logo.svg' style={{width:"50%"}}></img>
+                </div>
+                <div className='col'>
+                    <p>Account</p>
+                <a href=''>Open demat account</a>    
+                <a href=''>Minor demat account</a>    
+                <a href=''> NRI demat account</a>   
+                <a href=''> Commodity</a>   
+                <a href=''> Dematerialisation</a>   
+                <a href=''> Fund transfer</a>   
+                <a href=''> MTF</a>   
+                <a href=''> Referral program</a>   
+                </div>
+                <div className='col'>
+                    <p>Support</p>
+                </div>
+                <div className='col'>
+                    <p>Compeny</p>
+                </div>
+                <div className='col'>
+                    <p>Quick links</p>
+                </div>
+
+            </div>
+        </div>
      );
 }
 
