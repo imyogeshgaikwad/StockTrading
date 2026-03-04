3874a9df503129f16edc7cbae22c2dbaca307bd5
BEAST <imyogeshgaikwad@gmail.com>
2026-03-04 20:08:41 +0000

    Done with Support Page and its functionality

--- frontend/src/landing_page/support/CreateTicket.jsx
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -1,10 +1,168 @@
-import React from 'react';
+import React from "react";
 function CreateTicket() {
-    return ( 
-        <div>
-            <h1>Create Ticket</h1>
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">To create ticket, select a relavant topic</h1>
+
+        <div className="col-4 p-5 " style={{ textAlign: "left" }}>
+          <h4 className="">
+            <i class="fa-solid fa-circle-plus"></i> Account Opening
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Resident individual{" "}
+          </a>{" "}
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Minor
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Company, Partnership, HUF and LLP
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Glossary
+          </a>
+          <br />
         </div>
-     );
+
+        <div className="col-4 p-5 " style={{ textAlign: "left" }}>
+          <h4 className="">
+            <i class="fa-solid fa-circle-plus"></i> Your Zerodha Account
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Your Profile{" "}
+          </a>{" "}
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Account modification
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Client Master Report (CMR) and Depository Participant (DP)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Nomination
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Transfer and conversion of securities
+          </a>
+          <br />
+        </div>
+
+        <div className="col-4 p-5 " style={{ textAlign: "left" }}>
+          <h4 className="">
+            <i class="fa-solid fa-circle-plus"></i> Kite
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            IPO{" "}
+          </a>{" "}
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Trading FAQs
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Margin Trading Facility (MTF) and Margins
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Charts and orders
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Alerts and Nudges
+          </a>
+          <br />
+        </div>
+      </div>
+
+      {/* Second Section */}
+
+      <div className="row p-5 ">
+        <div className="col-4  " style={{ textAlign: "left" }}>
+          <h4 className="">
+            <i class="fa-solid fa-circle-plus"></i> Funds
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Add money{" "}
+          </a>{" "}
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Withdraw money
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Add bank accounts
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            eMandates
+          </a>
+          <br />
+        </div>
+
+        <div className="col-4  " style={{ textAlign: "left" }}>
+          <h4 className="">
+            <i class="fa-solid fa-circle-plus"></i> Console
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Portfolio{" "}
+          </a>{" "}
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Corporate actions
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Funds statement
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Reports
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Segments
+          </a>
+          <br />
+        </div>
+
+        <div className="col-4 " style={{ textAlign: "left" }}>
+          <h4 className="">
+            <i class="fa-solid fa-circle-plus"></i> Coin
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Mutual funds{" "}
+          </a>{" "}
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Fixed Deposit (FD)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Features on Coin
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            Payments and Orders
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2" }}>
+            General
+          </a>
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from "react";
 function Hero() {
-    return ( 
-        <div>
-            <h1>Hero</h1>
+  return (
+    <section className="container-fluid" id="support">
+      <div className="p-5 mt-5 mb-5" id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="">Track Tickets</a>
+      </div>
+
+      <div className=" row p-5 mx">
+        <div className=" col-6  ">
+          <h1 className="fs-3">
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+          <input placeholder="Eg.how to activate F&O"></input>
+          <a href="">1. Track account opening </a>
+          <a href="">2. Track segment activation</a>
+          <a href="">3. Intraday margins</a>
+          <a href="">4. Kite user manual</a>
+          <a href="">5. Learn how to create a ticket</a>
         </div>
-     );
+        <div className=" col-6  ">
+          <h1>Featured </h1>
+          <ol>
+            <li>
+              <a href="">Surveillance measure on scrips - March 2026</a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages and Square-off timings</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
